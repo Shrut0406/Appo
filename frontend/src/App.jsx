f3aa9dc7b4848c24f64fe6fb040e8c11b12128a3
Shrut0406 <bt22btech11015@iith.ac.in>
2025-08-09 11:50:14 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Doctor from './pages/Doctor'
+import Contact from './pages/Contact'
+import MyAppointments from './pages/MyAppointments'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/> 
+      
+      <Routes>
+        
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/doctor' element={<Doctor/>}/>
+        <Route path='/doctor/:speciality' element={<Doctor/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/my-appointments' element={<MyAppointments/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
+        
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/MyAppointments.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Appointments = () => {
+  return (
+    <div>
+      Appointments
+      
+    </div>
+  )
+}
+
+export default Appointments
